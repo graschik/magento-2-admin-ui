356bc287a4b421d8622102d78ff4cf2395f09a43
graschik <alexandr.grashchenko@gmail.com>
2022-11-28 19:52:47 +0100

    Add an example of using UI Components inside widgets

---
+++ Block/Widget/ExampleOfUIComponents.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Grasch\AdminUi\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class ExampleOfUIComponents extends Template implements BlockInterface
+{
+}
